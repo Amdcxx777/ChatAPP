f13ca5f91b9b08e9c60f4d3b26ab3eac44392f9a
Anthony <57707893+Amdcxx777@users.noreply.github.com>
2020-04-14 15:34:45 +0300

    renew

--- app/src/main/java/com/amdc/firebasetest/FilesViewerActivity.java
+++ app/src/main/java/com/amdc/firebasetest/FilesViewerActivity.java
@@ -11,6 +11,7 @@ import android.text.Html;
 import android.view.View;
 import android.webkit.WebChromeClient;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
@@ -49,7 +50,7 @@ public class FilesViewerActivity extends AppCompatActivity {
             DownloadManager.Request request = new DownloadManager.Request(uri);
             request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
             request.setDestinationInExternalFilesDir(FilesViewerActivity.this, DIRECTORY_DOWNLOADS, fileName);
-            Objects.requireNonNull(downloadManager).enqueue(request);
+            downloadManager.enqueue(request);
             lookingFile(uri);
         }).addOnFailureListener(e -> Toast.makeText(this, "Error download", Toast.LENGTH_SHORT).show());
     }
@@ -57,8 +58,7 @@ public class FilesViewerActivity extends AppCompatActivity {
     @SuppressLint("SetJavaScriptEnabled")
     private void lookingFile(Uri uri) {
         progressBar.setVisibility(View.VISIBLE);
-//        Toast.makeText(FilesViewerActivity.this, uri.toString(), Toast.LENGTH_LONG).show();
-//        String finalURL = "https://drive.google.com/viewerng/viewer?embedded=true&url=" + uri;
+        String finalURL = "https://drive.google.com/viewerng/viewer?embedded=true&url=" + uri; //https://drive.google.com/viewerng/viewer?embedded=true&url=  // https://docs.google.com/gview?key=YOUR_API_KEY&embedded=true&url=
         webView.getSettings().setJavaScriptEnabled(true);
         webView.getSettings().setBuiltInZoomControls(true);
         webView.setWebChromeClient(new WebChromeClient() {
@@ -72,104 +72,6 @@ public class FilesViewerActivity extends AppCompatActivity {
                 }
             }
         });
-        webView.loadUrl("https://drive.google.com/viewerng/viewer?embedded=true&url=" + uri);
+        webView.loadUrl(finalURL);
     }
 }
-
-
-//
-//
-////        String imageUrl =  getIntent().getStringExtra("url");
-////        Uri uri = Uri.parse(getIntent().getStringExtra("url"));
-////        Toast.makeText(FilesViewerActivity.this, uri.toString(), Toast.LENGTH_LONG).show();
-//        String file = getIntent().getStringExtra("url");
-////        Uri uri = Uri.parse("https://firebasestorage.googleapis.com/v0/b/testbase-def93.appspot.com/o/Document%20Files%2F-M4_GQJv-IpdmhC17GBm.pdf?alt=media&token=666eefe8-4124-4940-8786-6fd39d866d69");
-//        lookingFile(file);
-//    }
-//
-////    private void download() {
-//////        storageReference = FirebaseStorage.getInstance().getReference();
-//////        reference = storageReference.child("Document Files").child(uri);
-////        reference.getDownloadUrl().addOnSuccessListener(uri -> {
-////            DownloadManager downloadManager = (DownloadManager) FilesViewerActivity.this.getSystemService(Context.DOWNLOAD_SERVICE);
-////            DownloadManager.Request request = new DownloadManager.Request(uri);
-////            request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
-////            request.setDestinationInExternalFilesDir(FilesViewerActivity.this, DIRECTORY_DOWNLOADS, reference.getName());
-////            downloadManager.enqueue(request);
-////            lookingFile(uri);
-////        }).addOnFailureListener(e -> {
-////            Toast.makeText(this, "Error download", Toast.LENGTH_SHORT).show();
-////        });
-////    }
-//
-//    @SuppressLint("SetJavaScriptEnabled")
-//    private void lookingFile(String file) {
-////        Uri uri = Uri.parse(file);
-//        Toast.makeText(FilesViewerActivity.this, file, Toast.LENGTH_SHORT).show();
-//        progressBar.setVisibility(View.VISIBLE);
-//        String finalURL = "https://drive.google.com/viewerng/viewer?embedded=true&url=" + file;
-////        String finalURL = "https://stackoverflow.com/questions/8017374/how-to-pass-a-uri-to-an-intent";
-//        webView.getSettings().setJavaScriptEnabled(true);
-//        webView.getSettings().setBuiltInZoomControls(true);
-//        webView.setWebChromeClient(new WebChromeClient() {
-//            @Override
-//            public void onProgressChanged(WebView view, int newProgress) {
-//                super.onProgressChanged(view, newProgress);
-//                getSupportActionBar().setTitle("Loading...");
-//                if (newProgress == 100) { //indicated that loading is complite
-//                    progressBar.setVisibility(View.GONE);
-//                    getSupportActionBar().setTitle(Html.fromHtml("<font color='#F3FB00'>" + "File Viewer" + "</font>"));
-//                }
-//            }
-//        });
-//        webView.loadUrl(finalURL);
-//    }
-//}
-
-
-//        webView = findViewById(R.id.webView);
-//        progressBar = findViewById(R.id.progressBar);
-//        progressBar.setVisibility(View.GONE);
-//        String imageUrl = getIntent().getStringExtra("url");
-//        progressBar.setVisibility(View.VISIBLE);
-//        String finalURL = "https://drive.google.com/viewerng/viewer?embedded=true&url=" + imageUrl;
-//        webView.getSettings().setJavaScriptEnabled(true);
-//        webView.getSettings().setBuiltInZoomControls(true);
-//        webView.setWebChromeClient(new WebChromeClient(){
-//            @Override
-//            public void onProgressChanged(WebView view, int newProgress) {
-//                super.onProgressChanged(view, newProgress);
-//                getSupportActionBar().setTitle("Loading...");
-//                if (newProgress == 100) { //indicated that loading is complite
-//                    progressBar.setVisibility(View.GONE);
-//                    getSupportActionBar().setTitle(R.string.app_name);
-//                }
-//            }
-//        });
-//        webView.loadUrl(finalURL);
-//
-//    }
-
-
-
-//    @SuppressLint("SetJavaScriptEnabled")
-//    void lookingFile(Uri uri){
-//        progressBar.setVisibility(View.VISIBLE);
-////        Toast.makeText(this, uri, Toast.LENGTH_LONG).show();
-//        String finalURL = "https://drive.google.com/viewerng/viewer?embedded=true&url=" + uri;
-//        webView.getSettings().setJavaScriptEnabled(true);
-//        webView.getSettings().setBuiltInZoomControls(true);
-//        webView.setWebChromeClient(new WebChromeClient(){
-//            @Override
-//            public void onProgressChanged(WebView view, int newProgress) {
-//                super.onProgressChanged(view, newProgress);
-//                getSupportActionBar().setTitle("Loading...");
-//                if (newProgress == 100) { //indicated that loading is complite
-//                    progressBar.setVisibility(View.GONE);
-//                    getSupportActionBar().setTitle(R.string.app_name);
-//                }
-//            }
-//        });
-//        webView.loadUrl(finalURL);
-//    }
-//}
